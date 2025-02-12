337b91dee28142a0e282e58185f9567f12f1f2c7
hadri96 <68434862+hadri96@users.noreply.github.com>
2025-02-12 16:56:41 +0100

    Merging changes with master

--- endless_line/data_utils/dashboard_utils.py
+++ endless_line/data_utils/dashboard_utils.py
@@ -1,25 +1,23 @@
 from endless_line.data_utils.dataloader import DataLoader
-from endless_line.data_utils.dataloader import DataLoader
 from datetime import datetime
-
 class DashboardUtils:
-    """
-    DashboardUtils class provides utility functions to interact with the data loaded by DataLoader.
+	"""
+	DashboardUtils class provides utility functions to interact with the data loaded by DataLoader.
 
-    Methods
-    -------
-    get_attractions() -> list:
-        Returns a list of unique attractions
-    """
+	Methods
+	-------
+	get_attractions() -> list:
+		Returns a list of unique attractions
+	"""
 
-    def __init__(self):
-        self.data = DataLoader(db=True)
+	def __init__(self):
+		self.data = DataLoader(db=True)
 
-    def get_attractions(self):
-        self.data.link_attraction_park = self.data.load_file('link_attraction_park.csv')
-        self.data.clean_link_attraction_park()
-        return list(self.data.link_attraction_park.ATTRACTION.unique())
+	def get_attractions(self):
+		self.data.link_attraction_park = self.data.load_file('link_attraction_park.csv')
+		self.data.clean_link_attraction_park()
+		return list(self.data.link_attraction_park.ATTRACTION.unique())
 
-    def get_attendance(self, df, date: datetime.date):
-        output = df[df['ds'].dt.date.astype(str) == date]['yhat'].values[0]
-        return int(str(int(output)).replace(',', ' '))
+	def get_attendance(self, df, date: datetime.date):
+		output = df[df['ds'].dt.date.astype(str) == date]['yhat'].values[0]
+		return int(str(int(output)).replace(',', ' '))
